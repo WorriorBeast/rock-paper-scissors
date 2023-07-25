782b2778dc1f9f840aa8c2c8c1cfeca5e6f50ea0
WorriorBeast <128549074+WorriorBeast@users.noreply.github.com>
2023-07-25 13:20:23 -0700

    Declares round winner and simplified getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,16 +1,45 @@
+let computerChoice = Math.floor(Math.random() * 3);
+
 function getComputerChoice() {
-    let computerSelection = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {
+        return 'rock';
 
-    if (computerSelection < 1) {
-        return computerSelection = 'rock';
-
-    } else if (computerSelection >= 1 && computerSelection < 2){
-        return computerSelection = 'paper';
+    } else if (computerChoice === 1) {
+        return 'paper';
 
     } else {
-        return computerSelection = 'scissor';
+        return 'scissor';
     }
-}    
+}
 
-    let playerSelection = prompt('What will you choose: rock, paper, or scissors?', '').toLowerCase();
+let computerSelection = getComputerChoice();
+
+let playerSelection = prompt('What will you choose: rock, paper, or scissor?', '').toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+    let gameWinner;
+ 
+    if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        return gameWinner = alert('You are the round winner! Rock beats scissor.');
+
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return gameWinner = alert('You are the round winner! Paper beats rock.');
+
+    } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+        return gameWinner = alert('You are the round winner! Scissor beats paper.');
+
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return gameWinner = alert('Good luck next round! Paper beats rock.');
+
+    } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
+        return gameWinner = alert('Good luck next round! Scissor beats paper.');
     
+    } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+        return gameWinner = alert('Good luck next round! Rock beats scissor.');
+
+    } else {
+        return gameWinner = alert('It\'s a tie!');
+    }
+}
+
+playRound(playerSelection, computerSelection);
