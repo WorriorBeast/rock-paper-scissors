79288dc206d80b1950c6aec5807fd3c112119289
WorriorBeast <128549074+WorriorBeast@users.noreply.github.com>
2023-08-16 15:53:07 -0700

    Moved prompt inside of playRound function

--- javascript.js
+++ javascript.js
@@ -1,42 +1,43 @@
 let computerChoice = Math.floor(Math.random() * 3);
 
-function getComputerChoice() {
+function getComputerChoice(computerChoice) {
     if (computerChoice === 0) {
         return 'rock';
 
     } else if (computerChoice === 1) {
         return 'paper';
-
+  
     } else {
         return 'scissor';
     }
 }
 
-let computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice(computerChoice);
 
-let playerSelection = prompt('What will you choose: rock, paper, or scissor?', '').toLowerCase();
+let playerSelection = '';
 
 function playRound(playerSelection, computerSelection) {
     let gameWinner;
- 
+    playerSelection = prompt('What will you choose:\nrock, paper, or scissor?', playerSelection).toLowerCase();
+
     if (playerSelection === 'rock' && computerSelection === 'scissor') {
         return gameWinner = alert('You are the round winner! Rock beats scissor.');
-
+        
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         return gameWinner = alert('You are the round winner! Paper beats rock.');
-
+       
     } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
-        return gameWinner = alert('You are the round winner! Scissor beats paper.');
-
+       return gameWinner = alert('You are the round winner! Scissor beats paper.');
+        
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         return gameWinner = alert('Good luck next round! Paper beats rock.');
-
+        
     } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
         return gameWinner = alert('Good luck next round! Scissor beats paper.');
-    
+
     } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
         return gameWinner = alert('Good luck next round! Rock beats scissor.');
-
+        
     } else {
         return gameWinner = alert('It\'s a tie!');
     }
