0403caa036fa38e8fb4ab27434028c05c2ac93f3
WorriorBeast <128549074+WorriorBeast@users.noreply.github.com>
2023-08-16 16:55:01 -0700

    Play five rounds of rock, paper, and scissors

--- javascript.js
+++ javascript.js
@@ -17,30 +17,38 @@ let computerSelection = getComputerChoice(randomNum);
 let playerSelection = '';
 
 function playRound(playerSelection, computerSelection) {
-    let gameWinner;
     playerSelection = prompt('What will you choose:\nrock, paper, or scissor?', playerSelection).toLowerCase();
 
     if (playerSelection === 'rock' && computerSelection === 'scissor') {
-        return gameWinner = alert('You are the round winner! Rock beats scissor.');
+        return alert('You are the round winner! Rock beats scissor.');
         
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return gameWinner = alert('You are the round winner! Paper beats rock.');
+        return alert('You are the round winner! Paper beats rock.');
        
     } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
-       return gameWinner = alert('You are the round winner! Scissor beats paper.');
+       return alert('You are the round winner! Scissor beats paper.');
         
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return gameWinner = alert('Good luck next round! Paper beats rock.');
+        return alert('Good luck next round! Paper beats rock.');
         
     } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
-        return gameWinner = alert('Good luck next round! Scissor beats paper.');
+        return alert('Good luck next round! Scissor beats paper.');
 
     } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
-        return gameWinner = alert('Good luck next round! Rock beats scissor.');
+        return alert('Good luck next round! Rock beats scissor.');
         
     } else {
-        return gameWinner = alert('It\'s a tie!');
+        return alert('It\'s a tie!');
     }
 }
 
-playRound(playerSelection, computerSelection);
+function game() {
+    for (i = 0; i < 5; i++) {
+        let randomNum = Math.floor(Math.random() * 3) + 1;
+        let computerSelection = getComputerChoice(randomNum);
+        console.log(randomNum);
+        console.log(getComputerChoice(randomNum));
+        playRound(playerSelection, computerSelection);
+    }
+}
+game();
