4cd3f0700bfb8952bea4429e543b61876ce88642
WorriorBeast <128549074+WorriorBeast@users.noreply.github.com>
2023-08-19 16:40:52 -0700

    Keep score of all five rounds

--- javascript.js
+++ javascript.js
@@ -15,39 +15,49 @@ function getComputerChoice(randomNum) {
 let computerSelection = getComputerChoice(randomNum);
 
 let playerSelection = '';
+let playerWinner = 0,
+    computerWinner = 0,
+    tie = 0;
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = prompt('What will you choose:\nrock, paper, or scissor?', playerSelection).toLowerCase();
 
     if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        playerWinner++;
         return alert('You are the round winner! Rock beats scissor.');
         
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerWinner++;
         return alert('You are the round winner! Paper beats rock.');
        
     } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+        playerWinner++;
        return alert('You are the round winner! Scissor beats paper.');
         
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerWinner++;
         return alert('Good luck next round! Paper beats rock.');
         
     } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
+        computerWinner++;
         return alert('Good luck next round! Scissor beats paper.');
 
     } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+        computerWinner++;
         return alert('Good luck next round! Rock beats scissor.');
         
     } else {
+        tie++;
         return alert('It\'s a tie!');
     }
 }
 
 function game() {
-    for (i = 0; i < 5; i++) {
+    for (i = 0; playerWinner + computerWinner + tie < 5; i++) {
         let randomNum = Math.floor(Math.random() * 3) + 1;
         let computerSelection = getComputerChoice(randomNum);
-        console.log(randomNum);
-        console.log(getComputerChoice(randomNum));
+        
+        getComputerChoice(randomNum);
         playRound(playerSelection, computerSelection);
     }
 }
